d51f10ab2ced906865f46071dc4766c5e8a3f86b
abuamar142 <amarjabon130@gmail.com>
2023-05-24 12:59:00 +0700

    membuat database model

--- myapp/dooit/admin.py
+++ myapp/dooit/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import User, Pengguna, Kategori, Transaksi
 
-# Register your models here.
+@admin.register(Pengguna)
+class PenggunaDooit(admin.ModelAdmin):
+    list_display = ('username', 'nama', 'saldo')
+    search_fields = ('username', 'nama')
+    
+@admin.register(Kategori)
+class KategoriDooit(admin.ModelAdmin):
+    list_display = ('nama_kategori', 'keterangan')
+    search_fields = ('nama_kategori', 'keterangan')
+
+@admin.register(Transaksi)
+class TransaksiDooit(admin.ModelAdmin):
+    list_display = ('pengguna', 'kategori', 'tanggal_transaksi', 'jenis_transaksi', 'nominal', 'deskripsi')
+    raw_id_fields = ('pengguna', 'kategori')
+    search_fields = ('pengguna', 'kategori', 'deskripsi')
+
--- myapp/dooit/models.py
+++ myapp/dooit/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Pengguna(models.Model):
+    id_pengguna = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    nama = models.CharField(max_length=255)
+    saldo = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ('id_pengguna',)
+
+    def __str__(self):
+        return self.nama
+
+class Kategori(models.Model):
+    id_kategori = models.AutoField(primary_key=True)
+    nama_kategori = models.CharField(max_length=255)
+    keterangan = models.TextField()
+
+    class Meta:
+        ordering = ('id_kategori',)
+
+    def __str__(self):
+        return self.nama_kategori
+
+class Transaksi(models.Model):
+    JENIS_TRANSAKSI_CHOICES = [
+        ('pemasukan', 'Pemasukan'),
+        ('pengeluaran', 'Pengeluaran'),
+    ]
+
+    id_transaksi = models.AutoField(primary_key=True)
+    pengguna = models.ForeignKey(Pengguna, on_delete=models.CASCADE)
+    tanggal_transaksi = models.DateField()
+    jenis_transaksi = models.CharField(max_length=255, choices=JENIS_TRANSAKSI_CHOICES)
+    kategori = models.ForeignKey(Kategori, on_delete=models.CASCADE)
+    deskripsi = models.TextField(blank=True)
+    nominal = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        ordering = ('tanggal_transaksi',)
+
+    def __str__(self):
+        return self.deskripsi
